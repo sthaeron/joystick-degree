635944d6e517dd1755b921954e6a806d13f849f1
Sebastian Thaeron <sebastian.thaeron@gmail.com>
2024-06-11 18:01:00 +0200

    Changed collect.py to work on my mac

--- code/data-interpreter/collect.py
+++ code/data-interpreter/collect.py
@@ -1,14 +1,14 @@
 import serial
 import time
 filename = input('Please insert an output file name: \n')
-port = '/dev/ttyUSB0'
+port = '/dev/tty.usbserial-A50285BI'
 baudrate = 115200
 ser = serial.Serial(port, baudrate)
 print("portname:", ser.port)
 outfile = open(filename, "w")
 end_time = time.time() + 15
 while 1:
-    in_line = ser.readline().decode("utf-8")
+    in_line = ser.readline().decode("ascii")
     if in_line == "End of Transmission\r\n":
         print("reached break")
         break
