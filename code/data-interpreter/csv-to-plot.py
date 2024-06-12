e5f8553b209aa17c564be8ce83b5504c25507c2a
Sebastian Thaeron <sebastian.thaeron@gmail.com>
2024-06-12 13:13:24 +0200

    Working csv-to-plot script

--- code/data-interpreter/csv-to-plot.py
+++ code/data-interpreter/csv-to-plot.py
@@ -1,12 +1,16 @@
 #! /usr/local/bin/env python3
 import matplotlib.pyplot as plt 
 import csv 
+import sys
+
+controller = sys.argv[1]
+file = sys.argv[2]
 
 time = []
 x = []
 y = []
 
-with open("joystick-test.csv", "r") as csvfile:
+with open(file, "r") as csvfile:
     plots = csv.reader(csvfile, delimiter = ",")
 
     for row in plots:
@@ -14,16 +18,14 @@ with open("joystick-test.csv", "r") as csvfile:
         x.append(float(row[1]))
         y.append(float(row[2]))
 
-(xPlot, yPlot) = plt.subplots(2, 1, sharex=True)
+fig, (xPlot, yPlot) = plt.subplots(2, 1, sharex=True)
 
 xPlot.plot(time, x)
-xPlot.set_ylim([-1, 1])
-xPlot.xlabel("Time (ms)") 
+xPlot.set_xlabel("Time (ms)") 
 xPlot.set_ylabel("Y ADC Value")
 
 yPlot.plot(time, y)
-xPlot.set_ylim([-1, 1])
-yPlot.xlabel("Time (ms)") 
+yPlot.set_xlabel("Time (ms)") 
 yPlot.set_ylabel("Y ADC Value")
 
 plt.show()
