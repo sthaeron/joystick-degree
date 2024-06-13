2821ff9a8a1607c0a94c2c3861810acdf47a9d2c
Sebastian Thaeron <sebastian.thaeron@gmail.com>
2024-06-13 15:22:36 +0200

    Finalised csv-to-plot script

--- code/data-interpreter/csv-to-plot.py
+++ code/data-interpreter/csv-to-plot.py
@@ -28,17 +28,31 @@ with open(file, "r") as csvfile:
 
 fig, (xPlot, yPlot) = plt.subplots(2, 1, sharex=True)
 
+fig.set_figheight(4.25)
+fig.set_figwidth(7)
+
 xPlot.plot(time, x)
 xPlot.set_xlim(0, end)
-xPlot.set_ylim(-1, 1)
+xPlot.set_ylim(-1.1, 1.1)
+xPlot.spines["top"].set_visible(False)
+xPlot.spines["right"].set_visible(False)
+xPlot.axhline(0, color="grey", linewidth="0.8", linestyle="--")
+xPlot.xaxis.set_tick_params(labelbottom=True)
 xPlot.set_xlabel("Time (ms)")
 xPlot.set_ylabel("X-Axis Value")
 
-
 yPlot.plot(time, y)
 yPlot.set_xlim(0, end)
-yPlot.set_ylim(-1, 1)
+yPlot.set_ylim(-1.1, 1.1)
+yPlot.spines["top"].set_visible(False)
+yPlot.spines["right"].set_visible(False)
+yPlot.axhline(0, color="grey", linewidth="0.8", linestyle="--")
+yPlot.xaxis.set_tick_params(labelbottom=True)
 yPlot.set_xlabel("Time (ms)")
 yPlot.set_ylabel("Y-Axis Value")
 
-plt.show()
+plt.tight_layout()
+filepath = file.split('/')
+name = filepath[len(filepath)-1].split('.')[0] + ".svg"
+print(name)
+plt.savefig(name)
