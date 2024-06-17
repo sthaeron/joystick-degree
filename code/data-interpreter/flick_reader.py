9be2a2f4c118037c1730ac632c8f16feeededb4a
signed-kristoffer <ksban@kth.se>
2024-06-17 18:50:14 +0200

    mostly finalized flick_reader.py

--- code/data-interpreter/flick_reader.py
+++ code/data-interpreter/flick_reader.py
@@ -1,7 +1,10 @@
 import sys
 import csv
 from numpy import mean
-
+print("Quick how to use:")
+print("argv[1] is csv to read")
+print("argv[2] is origin for x (like 0), and argv[3] is origin for y (like)")
+print("example execute: python3.11 flick_reader.py dreamcast_flick_1.csv 0 0")
 file = sys.argv[1]
 
 time = []
@@ -25,8 +28,8 @@ with open(file, "r") as csvfile:
         x.append(float(row[1]))
         y.append(float(row[2]))
 
-origin = 0
-origin_range = 0.4
+origin = float(sys.argv[2])
+origin_range = 0.3
 in_flick = 0
 flick_list_list = []
 x_flick_list = [[]]
@@ -47,6 +50,8 @@ for i in x:
         in_flick = 0
         k = k + 1
 k = 0
+if sys.argv[3]:
+    origin = float(sys.argv[3])
 for i in y:
     if i < origin - origin_range:
         if in_flick == 0:
@@ -85,14 +90,25 @@ for i in y_flick_list:
     k = k + 1
     y_min_max_mean.append([])
 k = 0
-for i in x_min_max_mean:
-    if i == []: continue
-    print("X flick #", str(k) ," min: " + str(i[0]) + ", max: " + str(i[1]) + ", mean: " + str(i[2]))
-    k = k + 1
-print("")
-k = 0
-for i in y_min_max_mean:
-    if i == []: continue
-    print("Y flick #", str(k) ," min: " + str(i[0]) + ", max: " + str(i[1]) + ", mean: " + str(i[2]))
-    k = k + 1
 
+print("Flick trial x, X min, X max, X mean, Y min, Y max, Y mean")
+print("Flick #1, -, -, -, ", y_min_max_mean[0][0], ", ", y_min_max_mean[0][1], ", ", y_min_max_mean[0][2])
+print("Flick #2, ", x_min_max_mean[0][0], ", ", x_min_max_mean[0][1], ", ", x_min_max_mean[0][2], ", ", y_min_max_mean[0][0], ", ", y_min_max_mean[0][1], ", ", y_min_max_mean[0][2])
+print("Flick #3, ", x_min_max_mean[0][0], ", ", x_min_max_mean[0][1], ", ", x_min_max_mean[0][2], ", ", '-', ", ", '-', ", ", '-')
+print("Flick #4, ", x_min_max_mean[0][0], ", ", x_min_max_mean[0][1], ", ", x_min_max_mean[0][2], ", ", y_min_max_mean[0][0], ", ", y_min_max_mean[0][1], ", ", y_min_max_mean[0][2])
+print("Flick #5, -, -, -, ", y_min_max_mean[0][0], ", ", y_min_max_mean[0][1], ", ", y_min_max_mean[0][2])
+print("Flick #6, ", x_min_max_mean[0][0], ", ", x_min_max_mean[0][1], ", ", x_min_max_mean[0][2], ", ", y_min_max_mean[0][0], ", ", y_min_max_mean[0][1], ", ", y_min_max_mean[0][2])
+print("Flick #7, ", x_min_max_mean[0][0], ", ", x_min_max_mean[0][1], ", ", x_min_max_mean[0][2], ", ", '-', ", ", '-', ", ", '-')
+print("Flick #8, ", x_min_max_mean[0][0], ", ", x_min_max_mean[0][1], ", ", x_min_max_mean[0][2], ", ", y_min_max_mean[0][0], ", ", y_min_max_mean[0][1], ", ", y_min_max_mean[0][2])
+#
+# for i in x_min_max_mean:
+#     if i == []: continue
+#     print("X flick #", str(k) ," min: " + str(i[0]) + ", max: " + str(i[1]) + ", mean: " + str(i[2]))
+#     k = k + 1
+# print("")
+# k = 0
+# for i in y_min_max_mean:
+#     if i == []: continue
+#     print("Y flick #", str(k) ," min: " + str(i[0]) + ", max: " + str(i[1]) + ", mean: " + str(i[2]))
+#     k = k + 1
+#
