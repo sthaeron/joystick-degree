1841f2a8fcaf55124a930f8f3583666775fd56ed
signed-kristoffer <ksban@kth.se>
2024-06-17 17:50:44 +0200

    working flick reader for human readable

--- code/data-interpreter/flick_reader.py
+++ code/data-interpreter/flick_reader.py
@@ -30,7 +30,7 @@ origin_range = 0.4
 in_flick = 0
 flick_list_list = []
 x_flick_list = [[]]
-y_flick_list = []
+y_flick_list = [[]]
 k = 0
 for i in x:
     if i < origin - origin_range:
@@ -47,14 +47,52 @@ for i in x:
         in_flick = 0
         k = k + 1
 k = 0
+for i in y:
+    if i < origin - origin_range:
+        if in_flick == 0:
+            in_flick = 1
+            y_flick_list.append([])
+        y_flick_list[k].append(i)
+    elif i > origin + origin_range:
+        if in_flick == 0:
+            in_flick = 1
+            y_flick_list.append([])
+        y_flick_list[k].append(i)
+    elif in_flick == 1:
+        in_flick = 0
+        k = k + 1
+
+
+k = 0
 min([], default="EMPTY")
-min_max_mean = [[]]
+x_min_max_mean = [[]]
 for i in x_flick_list:
     if i == []:
         continue
-    min_max_mean[k].append(min(i))
-    min_max_mean[k].append(max(i))
-    min_max_mean[k].append(mean(i))
+    x_min_max_mean[k].append(min(i))
+    x_min_max_mean[k].append(max(i))
+    x_min_max_mean[k].append(mean(i))
     k = k + 1
-    min_max_mean.append([])
-print(min_max_mean)
+    x_min_max_mean.append([])
+y_min_max_mean = [[]]
+k = 0
+for i in y_flick_list:
+    if i == []:
+        continue
+    y_min_max_mean[k].append(min(i))
+    y_min_max_mean[k].append(max(i))
+    y_min_max_mean[k].append(mean(i))
+    k = k + 1
+    y_min_max_mean.append([])
+k = 0
+for i in x_min_max_mean:
+    if i == []: continue
+    print("X flick #", str(k) ," min: " + str(i[0]) + ", max: " + str(i[1]) + ", mean: " + str(i[2]))
+    k = k + 1
+print("")
+k = 0
+for i in y_min_max_mean:
+    if i == []: continue
+    print("Y flick #", str(k) ," min: " + str(i[0]) + ", max: " + str(i[1]) + ", mean: " + str(i[2]))
+    k = k + 1
+
